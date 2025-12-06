3d180a6a5b4983d3d3c3d0732840953104d2fa30
sri <srivagdevikandala01@gmail.com>
2025-12-06 21:42:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    """
-    Simple home endpoint.
-    """
     return "Hello from Python Static Site!", 200
 
 
 @app.route("/health")
 def health():
-    """
-    Health check endpoint.
-    """
     return jsonify(status="ok"), 200
 
 
 if __name__ == "__main__":
-    # Dev mode; in Docker we’ll use gunicorn
     app.run(host="0.0.0.0", port=8000)
